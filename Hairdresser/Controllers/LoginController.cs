368c554adda0bda799b024e44efa2d2817d88ad7
Huseyinakbal39 <huseyin.akbal@ogr.sakarya.edu.tr>
2024-12-09 18:19:06 +0300

    Login page added

---
+++ Hairdresser/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Hairdresser.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hairdresser.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Login İşlemini Yap
+        [HttpPost]
+        public IActionResult Login(UserModel user)
+        {
+            // Basit bir kullanıcı doğrulama
+            if (user.Username == "g211210069@sakarya.edu.tr" && user.Password == "sau")
+            {
+                // Başarılı giriş
+                TempData["Message"] = "Giriş başarılı!";
+                return RedirectToAction("Welcome");
+            }
+
+            // Hatalı giriş
+            ViewBag.ErrorMessage = "Kullanıcı adı veya şifre hatalı.";
+            return RedirectToAction("Index");
+        }
+
+        // Hoşgeldiniz Sayfası
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
