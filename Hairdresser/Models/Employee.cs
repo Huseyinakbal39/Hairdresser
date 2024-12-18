9463065ba86ff07c0780ed7161b31cd82d1be74c
Huseyinakbal39 <huseyin.akbal@ogr.sakarya.edu.tr>
2024-12-18 23:12:06 +0300

    Admin Detail

--- Hairdresser/Models/Employee.cs
+++ Hairdresser/Models/Employee.cs
@@ -4,6 +4,7 @@ namespace Hairdresser.Models
 {
     public class Employee
     {
+        [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Please enter a valid name")]
         [MaxLength(50)]
@@ -18,13 +19,14 @@ namespace Hairdresser.Models
         [EmailAddress(ErrorMessage = "Please enter a valid email")]
         public string Email { get; set; }
         [Phone(ErrorMessage = "Please enter a valid phone number")]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public string Position { get; set; }
         [Required(ErrorMessage = "Please enter a valid salary")]
         [Display(Name = "Employee Salary")]
         [Range(1000,100000,ErrorMessage = "Salary must be between 1000-100000")]
         public int Salary { get; set; }
-        public ICollection<WorkingHours> WorkingHours { get; set; } // Çalışma saatleri
-        public ICollection<Appointment> Appointments { get; set; } // Çalışanın gerçekleştirdiği randevular
+        //public ICollection<WorkingHours> WorkingHours { get; set; } // Çalışma saatleri
+        //public ICollection<Appointment> Appointments { get; set; } // Çalışanın gerçekleştirdiği randevular
     }
 }
